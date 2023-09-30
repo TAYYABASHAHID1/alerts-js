336f19eaf62c7701d50231c82a41c5b165dc4d99
Tayyaba Shahid <136459395+TAYYABASHAHID1@users.noreply.github.com>
2023-09-30 11:25:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("ERROR! please enter a valid password")
+
+
+
+alert("welcome to JS land....\nhappy coding");
+
+
+
+alert("welcome to js land.....")
+
+alert("happy coding...")
+
+alert("hello I can run JS through browser console ")
